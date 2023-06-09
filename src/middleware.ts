bb5fc8fc7c149944b05cc42a097d07784e248a98
AlexJamisone <business.homeit@gmail.com>
2023-06-09 13:16:13 +0300

    add rateLimit to authmiddlewere

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,23 @@
 import { authMiddleware } from '@clerk/nextjs';
-export default authMiddleware();
+import { NextResponse } from 'next/server';
+import { ratelimit } from './server/helpers/ratelimit';
+export default authMiddleware({
+	async afterAuth(auth, req, evt) {
+		const ip = req.ip ?? '127.0.0.1';
+		const { success, pending, limit, reset, remaining } =
+			await ratelimit.baseAll.limit(`ratelimit_middleware_${ip}`);
+		evt.waitUntil(pending);
+
+		const res = success
+			? NextResponse.next()
+			: NextResponse.redirect(new URL('/api/blocked', req.url));
+
+		res.headers.set('X-RateLimit-Limit', limit.toString());
+		res.headers.set('X-RateLimit-Remaining', remaining.toString());
+		res.headers.set('X-RateLimit-Reset', reset.toString());
+		return res;
+	},
+});
 
 export const config = {
 	matcher: ['/((?!_next/image|_next/static|favicon.ico).*)', '/api/:path*'],
