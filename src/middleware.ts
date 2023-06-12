7362b78262b404b10d1004c8399bab975158c8fd
AlexJamisone <business.homeit@gmail.com>
2023-06-12 22:34:33 +0300

    change authMiddleware to withClerkMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,32 @@
-import { authMiddleware } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import { withClerkMiddleware } from '@clerk/nextjs/server';
+import {
+	NextResponse,
+	type NextFetchEvent,
+	type NextRequest,
+} from 'next/server';
 import { ratelimit } from './server/helpers/ratelimit';
-export default authMiddleware({
-	async afterAuth(auth, req, evt) {
-		const ip = req.ip ?? '127.0.0.1';
+
+export default withClerkMiddleware(
+	async (
+		request: NextRequest,
+		event: NextFetchEvent
+	): Promise<Response | undefined> => {
+		const ip = request.ip ?? '127.0.0.1';
+
 		const { success, pending, limit, reset, remaining } =
 			await ratelimit.baseAll.limit(`ratelimit_middleware_${ip}`);
-		evt.waitUntil(pending);
+		event.waitUntil(pending);
 
 		const res = success
 			? NextResponse.next()
-			: NextResponse.redirect(new URL('/api/blocked', req.url));
+			: NextResponse.redirect(new URL('/api/blocked', request.url));
 
 		res.headers.set('X-RateLimit-Limit', limit.toString());
 		res.headers.set('X-RateLimit-Remaining', remaining.toString());
 		res.headers.set('X-RateLimit-Reset', reset.toString());
 		return res;
-	},
-});
+	}
+);
 
 export const config = {
 	matcher: ['/((?!_next/image|_next/static|favicon.ico).*)', '/api/:path*'],
